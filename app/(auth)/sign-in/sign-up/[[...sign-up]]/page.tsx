884816bcd69c0aa2075851d9c5ee7ddb97b87a1b
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2024-12-03 23:09:24 +0530

    auth-added

---
+++ app/(auth)/sign-in/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+
+const SignInPage = () => {
+  return (
+    <main className="auth-page">
+      <SignUp />
+    </main>
+  )
+}
+
+export default SignInPage
--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,14 +1,24 @@
 import { Editor } from '@/components/ui/editor/Editor'
 import React from 'react'
 import Header from '@/components/Header'
+import { UserButton } from '@clerk/nextjs'
+import { SignedIn } from '@clerk/nextjs'
+import { SignedOut } from '@clerk/nextjs'
+import { SignInButton } from '@clerk/nextjs'
 
 const page = () => {
   return (
     <div>
       <Header  >
         <div>
-            This is document title
+            Share
         </div>
+        <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
